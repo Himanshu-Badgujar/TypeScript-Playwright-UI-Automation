f7b215e1b94ddc1d4c577ae0daf36afe977e4f1c
Himanshu Badgujar <h.spmid@gmail.com>
2025-06-10 14:18:53 +0200

    Added .env support

--- tests/e2eCheckout.spec.ts
+++ tests/e2eCheckout.spec.ts
@@ -1,12 +1,16 @@
 import {test, expect} from '@playwright/test';
+
 import {LoginPage} from '../pages/accountLogin';
-import { YourStore } from '../pages/yourStore';
-import { Checkout } from '../pages/checkout';
+import {YourStore} from '../pages/yourStore';
+import {Checkout} from '../pages/checkout';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 test('e2eCheckout', async({page})=>{
     const loginPage = new LoginPage(page);
     expect(await loginPage.gotoLoginPage()).toBe(true);
-    expect(await loginPage.doLogin('0123456789@gmail.com', 'testuser')).toBe(true);
+    expect(await loginPage.doLogin(process.env.emailID as string, process.env.password as string)).toBe(true);
 
     const yourStore = new YourStore(page);
     expect(await yourStore.gotoYourStore()).toBe(true);
@@ -16,13 +20,11 @@ test('e2eCheckout', async({page})=>{
 
     const checkout = new Checkout(page)
     expect (typeof await checkout.checkiWantToUseNewAddress()).toBe('boolean')
-    expect (await checkout.fillFirstName('Test')).toBe(true);
-    expect (await checkout.fillLastName('User')).toBe(true);
-    expect (await checkout.fillAddress('This is my address.')).toBe(true);
-    expect (await checkout.fillCity('This is my city.')).toBe(true);
-    expect (await checkout.fillPostcode('This is my postcode.')).toBe(true);
-    expect (await checkout.fillCountrySelector('81')).toBe(true);
-    expect (await checkout.fillRegionSelector('1254')).toBe(true);
-
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    expect (await checkout.fillFirstName(process.env.firstName as string)).toBe(true);
+    expect (await checkout.fillLastName(process.env.lastName as string)).toBe(true);
+    expect (await checkout.fillAddress(process.env.address as string)).toBe(true);
+    expect (await checkout.fillCity(process.env.city as string)).toBe(true);
+    expect (await checkout.fillPostcode(process.env.postcode as string)).toBe(true);
+    expect (await checkout.fillCountrySelector(process.env.country as string)).toBe(true);
+    expect (await checkout.fillRegionSelector(process.env.region as string)).toBe(true);
 });
